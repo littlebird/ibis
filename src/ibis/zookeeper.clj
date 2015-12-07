6cccfe527f553017c3a1a0704bd81a42e37c8088
Ryan Spangler <ryan@getlittlebird.com>
2015-12-07 13:23:37 -0800

    attempts zookeeper reconnect if session expires

--- src/ibis/zookeeper.clj
+++ src/ibis/zookeeper.clj
@@ -7,14 +7,36 @@
   [path]
   (str \/ (string/join "/" path)))
 
+(defn establish-connection
+  [host port]
+  (zookeeper/connect (str host \: port)))
+
 (defn connect
   [host port]
-  (zookeeper/connect
-   (str host \: port)))
+  (let [connection (establish-connection host port)]
+    {:connection (atom connection)
+     :host host
+     :port port}))
+
+(defn reconnect
+  [{:keys [host port connection]}]
+  (swap! connection (constantly (establish-connection host port))))
+
+(defn with-reconnect
+  [f zookeeper & args]
+  (try
+    (apply f (cons @(:connection zookeeper) args))
+    (catch Exception e
+      (reconnect zookeeper)
+      (apply f (cons @(:connection zookeeper) args)))))
 
 (defn exists?
   [zookeeper path]
-  (zookeeper/exists zookeeper (pathify path)))
+  (with-reconnect
+    (fn [connection path]
+      (zookeeper/exists connection (pathify path)))
+    zookeeper
+    path))
 
 (defn count-children
   [zookeeper path]
@@ -25,11 +47,18 @@
   (doseq [span (range 1 (inc (count path)))]
     (let [subpath (take span path)]
       (if-not (exists? zookeeper subpath)
-        (zookeeper/create zookeeper (pathify subpath) :persistent? true)))))
+        (zookeeper/create
+         @(:connection zookeeper)
+         (pathify subpath)
+         :persistent? true)))))
 
 (defn delete
   [zookeeper path]
-  (zookeeper/delete-all zookeeper (pathify path)))
+  (with-reconnect
+    (fn [connection path]
+      (zookeeper/delete-all connection (pathify path)))
+    zookeeper
+    path))
 
 (defn ibis-watcher
   [{:keys [event-type keeper-state path] :as event}]
