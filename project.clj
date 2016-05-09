246fe0ff0a44a77999b3a966bc11e89ccd30a7a6
Justin Glenn Smith <noisesmith@gmail.com>
2016-05-09 09:30:09 -0700

    preparation to start tracking active task segments

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject littlebird/ibis "0.0.25"
+(defproject littlebird/ibis "0.0.26"
   :description "Distribute jobs among any number of peers using Kafka"
   :url "http://github.com/littlebird/ibis"
   :license {:name "Eclipse Public License"
--- src/ibis/flock.clj
+++ src/ibis/flock.clj
@@ -23,67 +23,66 @@
       :segment-id segment-id})))
 
 (defn launch!
-  [{:keys [ibis-id transmit receive stages store update producer encoders pool]
+  [{:keys [ibis-id transmit receive stages store updater producer encoders pool]
     :as ibis}]
   (let [flock-id (java.util.UUID/randomUUID)]
     (log/trace "IBIS flock thread" flock-id "launched")
     (pool/future
       pool
       (loop [{:keys [journey stage message traveled segment-id] :as segment} (receive)]
-        (try
-          (if segment
-            (if (= stage :out)
-              (let [output (kafka/make-transmit producer (:topic journey) encoders)]
-                (output
-                 {:journey journey
-                  :stage :out
-                  :message message
-                  :traveled (conj traveled stage)
-                  :segment-id segment-id}))
-              (if-let [work (get stages stage)]
-                (let [stage-id (java.util.UUID/randomUUID)
-                      continuations (get-in journey [:course stage])]
-                  (store
-                   :stage
-                   (merge
-                    segment
-                    {:ibis-id ibis-id
-                     :journey-id (:id journey)
-                     :stage stage
-                     :stage-id stage-id
-                     :started (time/now)
-                     :status "running"}))
-                  (try
-                    (let [result
-                          (if (= message :land)
-                            :land
-                            (work (assoc message :ibis ibis)))]
-                      (update
-                       :stage {:stage-id stage-id}
-                       {:completed (time/now)
-                        :status "complete"})
-                      (passage
-                       transmit journey stage continuations
-                       (if (keyword? result)
-                         result
-                         (dissoc result :ibis))
-                       traveled segment-id))
-                    (catch Exception e
-                      (let [exception (serialize-exception e)]
-                        (log/error "Exception in stage" stage stage-id)
-                        (log/error (pprint/pprint exception))
-                        (update
-                         :stage {:stage-id stage-id}
-                         {:failed (time/now)
-                          :exception exception
-                          :status "failed"})
+        (when segment
+          (let [out-stage? (= stage :out)
+                work (get stages stage)])
+          (try
+            (cond out-stage?
+                  (let [output (kafka/make-transmit producer (:topic journey) encoders)]
+                    (output
+                      {:journey journey
+                       :stage :out
+                       :message message
+                       :traveled (conj traveled stage)
+                       :segment-id segment-id}))
+                  work
+                  (let [stage-id (java.util.UUID/randomUUID)
+                        continuations (get-in journey [:course stage])]
+                    (store
+                      :stage
+                      (assoc segment
+                             :ibis-id ibis-id
+                             :journey-id (:id journey)
+                             :stage stage
+                             :stage-id stage-id
+                             :started (time/now)
+                             :status "running"))
+                    (try
+                      (let [result
+                            (if (= message :land)
+                              :land
+                              (work (assoc message :ibis ibis)))]
+                        (updater :stage {:stage-id stage-id}
+                                 {:completed (time/now)
+                                  :status "complete"})
                         (passage
-                         transmit journey stage continuations
-                         {} traveled segment-id))))))))
-          (catch Exception e
-            (let [exception (serialize-exception e)]
-              (log/error "Exception during journey" (:id journey))
-              (log/error (pprint/pprint exception)))))
+                          transmit journey stage continuations
+                          (if (keyword? result)
+                            result
+                            (dissoc result :ibis))
+                          traveled segment-id))
+                      (catch Exception e
+                        (let [exception (serialize-exception e)]
+                          (log/error "Exception in stage" stage stage-id)
+                          (log/error (pprint/pprint exception))
+                          (updater :stage {:stage-id stage-id}
+                                   {:failed (time/now)
+                                    :exception exception
+                                    :status "failed"})
+                          (passage
+                            transmit journey stage continuations
+                            {} traveled segment-id))))))
+            (catch Exception e
+              (let [exception (serialize-exception e)]
+                (log/error "Exception during journey" (:id journey))
+                (log/error (pprint/pprint exception))))))
         (recur (receive))))))
 
 (defn launch-all!
